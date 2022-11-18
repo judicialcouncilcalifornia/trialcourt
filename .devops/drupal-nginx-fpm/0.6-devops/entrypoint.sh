f5865af077c863f398577bad3d8e685f11b3de8a
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-11-17 18:02:13 -0800

    build new docker image

---
+++ .devops/drupal-nginx-fpm/0.6-devops/entrypoint.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# set -e
+
+php -v
+
+#Get drupal from Git
+setup_drupal(){
+  while test -d "$DRUPAL_PRJ"
+  do
+    echo "INFO: $DRUPAL_PRJ is exist, clean it ..."
+    # mv is faster than rm.
+    mv $DRUPAL_PRJ /tmp/drupal_prj_bak$(date +%s)
+  done
+
+  test ! -d "$DRUPAL_PRJ" && echo "INFO: $DRUPAL_PRJ not found. creating..." && mkdir -p "$DRUPAL_PRJ"
+	cd $DRUPAL_PRJ
+
+  if [ -d "$DRUPAL_BUILD" ]
+  then
+    echo "Copying files from $DRUPAL_BUILD to $DRUPAL_PRJ"
+    mv $DRUPAL_BUILD/* $DRUPAL_PRJ/
+  else
+    	echo "INFO: ++++++++++++++++++++++++++++++++++++++++++++++++++:"
+    	echo "REPO: "$GIT_REPO
+    	echo "BRANCH: "$GIT_BRANCH
+    	echo "INFO: ++++++++++++++++++++++++++++++++++++++++++++++++++:"
+
+      echo "INFO: Clone from "$GIT_REPO
+      git clone ${GIT_REPO} --branch ${GIT_BRANCH} $DRUPAL_PRJ	&& cd $DRUPAL_PRJ
+      rm -rf $DRUPAL_PRJ/.git
+
+      composer install
+      scripts/theme.sh -i jcc_base && scripts/theme.sh -b jcc_base
+      scripts/theme.sh -i jcc_deprep && scripts/theme.sh -b jcc_deprep
+      scripts/theme.sh -i jcc_newsroom && scripts/theme.sh -b jcc_newsroom
+  fi
+
+
+  test ! -d "$DRUPAL_PRJ/web/sites/default/files" && mkdir -p "$DRUPAL_PRJ/web/sites/default/files"
+  chmod a+w "$DRUPAL_PRJ/web/sites/default"
+  chmod a+w "$DRUPAL_PRJ/web/sites/default/files"
+
+  # Tell code to use Azure Settings for all sites
+  find $DRUPAL_PRJ/web/sites -maxdepth 1 -mindepth 1 -type d | while read dir; do
+    SITE_ID=$(basename $dir)
+
+    test -d "$dir/settings.local.php" && chmod a+w "$dir/settings.local.php" && rm "$dir/settings.local.php"
+    cp "$DRUPAL_SOURCE/settings.local.php" "$dir/settings.local.php"
+    chmod a-w "$dir/settings.php"
+    chmod a-w "$dir/settings.local.php"
+  done
+
+  while test -d "$DRUPAL_HOME"
+  do
+      echo "INFO: $DRUPAL_HOME is exist, clean it ..."
+      chmod 777 -R $DRUPAL_HOME
+      rm -Rf $DRUPAL_HOME
+  done
+  ln -s $DRUPAL_PRJ/web $DRUPAL_HOME
+
+  # Persist drupal/sites
+  if [ -d "$DRUPAL_STORAGE" ]
+  then
+    test ! -d "$DRUPAL_STORAGE/files" && mkdir -p "$DRUPAL_STORAGE/files"
+    ln -s $DRUPAL_STORAGE/files $DRUPAL_PRJ/web/sites/default/files
+  else
+      echo "Error: Directory $DRUPAL_STORAGE is not mounted."
+  fi
+}
+
+echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
+
+# setup Drupal
+if [ -e "$DRUPAL_HOME/web/sites/default/settings.php" ] || [ "$RESET_INSTANCE" == "true" ];then
+# Site is exist.
+    if [ -d "$DRUPAL_PRJ" ]; then
+        echo "INFO: $DRUPAL_PRJ is exist..."
+        echo "INFO: Site is Ready..."
+    else
+        echo "Installing Drupal ..."
+        setup_drupal
+    fi
+else
+# drupal isn't installed, fresh start
+    echo "Installing Drupal ..."
+    setup_drupal
+fi
+
+# Create log folders
+test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
+test ! -d "$VARNISH_LOG_DIR" && echo "INFO: Log folder for varnish found. creating..." && mkdir -p "$VARNISH_LOG_DIR"
+test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for nginx/php not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
+test ! -e /home/50x.html && echo "INFO: 50x file not found. creating..." && cp /usr/share/nginx/html/50x.html /home/50x.html
+# Backup default nginx setting, use customer's nginx setting
+test -d "/home/etc/nginx" && mv /etc/nginx /etc/nginx-bak && ln -s /home/etc/nginx /etc/nginx
+test ! -d "/home/etc/nginx" && mkdir -p /home/etc && mv /etc/nginx /home/etc/nginx && ln -s /home/etc/nginx /etc/nginx
+# Backup default varnish setting, use customer's nginx setting
+test -d "/home/etc/varnish" && mv /etc/varnish /etc/varnish-bak && ln -s /home/etc/varnish /etc/varnish
+test ! -d "/home/etc/varnish" && mkdir -p /home/etc && mv /etc/varnish /home/etc/varnish && ln -s /home/etc/varnish /etc/varnish
+
+#echo "Starting Varnishd ..."
+if [ "$ENABLE_VARNISH" == "true" ];then
+  /usr/sbin/varnishd -a :80 -f /etc/varnish/default.vcl
+  sed -i 's|listen 80;|listen 8080;|g' /home/etc/nginx/nginx.conf
+  sed -i 's|listen [::]:80;|listen [::]:8080;|g' /home/etc/nginx/nginx.conf
+fi
+
+echo "INFO: creating /run/php/php7.0-fpm.sock ..."
+test -e /run/php/php7.0-fpm.sock && rm -f /run/php/php7.0-fpm.sock
+mkdir -p /run/php && touch /run/php/php7.0-fpm.sock && chown nginx:nginx /run/php/php7.0-fpm.sock && chmod 777 /run/php/php7.0-fpm.sock
+
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+
+echo "Starting SSH ..."
+echo "Starting php-fpm ..."
+echo "Starting Nginx ..."
+
+cd /usr/bin/
+supervisord -c /etc/supervisord.conf
