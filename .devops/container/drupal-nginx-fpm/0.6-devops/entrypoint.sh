20d39a2309dcfc5ef039bc65ef856333f2531fc1
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-12-20 09:33:58 -0800

    fix environment vars in shell

--- .devops/container/drupal-nginx-fpm/0.6-devops/entrypoint.sh
+++ .devops/container/drupal-nginx-fpm/0.6-devops/entrypoint.sh
@@ -110,7 +110,7 @@ mkdir -p /run/php && touch /run/php/php-fpm.sock && chown nginx:nginx /run/php/p
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 
 # Get environment variables to show up in SSH session
-eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 echo "Starting SSH ..."
 echo "Starting php-fpm ..."
--- .devops/container/drupal-nginx-fpm/0.6-devops/settings.local.php
+++ .devops/container/drupal-nginx-fpm/0.6-devops/settings.local.php
@@ -8,7 +8,12 @@
  * environment, but may be exposed if you migrate your site to
  * another environment.
  */
-$settings['file_private_path'] = 'sites/default/files/private/' . $_ENV['SITE_MAP_ID'];
+$config['system.file']['path']['temporary'] = '/home/tmp';
+$config['system.file']['path']['private'] = 'sites/default/files/private/' . $_ENV['SITE_MAP_ID'];
+$settings['file_temporary_path'] = $config['system.file']['path']['temporary'];
+$settings['file_private_path'] = $config['system.file']['path']['private'];
+$settings['php_storage']['twig']['directory'] = $config['system.file']['path']['temporary'];
+$settings['php_storage']['twig']['secret'] = $settings['hash_salt'] . $_ENV['SITE_MAP_ID'];
 
 $settings['reverse_proxy'] = TRUE;
 $settings['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
@@ -16,8 +21,8 @@ $settings['reverse_proxy_trusted_headers'] = \Symfony\Component\HttpFoundation\R
 $settings['trusted_host_patterns'][] = '.*';
 
 //  Store local configuration separately so it isn't tracked by git.
-$config['config_split.config_split.local']['status'] = TRUE;
-$config['config_split.config_split.stage']['status'] = TRUE;
+//$config['config_split.config_split.local']['status'] = TRUE;
+//$config['config_split.config_split.stage']['status'] = TRUE;
 
 $databases['default']['default'] = array(
     'database' => $_ENV["DATABASE_NAME"],
@@ -38,8 +43,8 @@ if (isset($_ENV['REDIS_HOST'])) {
   $settings['cache_prefix']['default'] = $_ENV['SITE_MAP_ID'];
   $settings['redis_compress_length'] = 100;
   $settings['redis_compress_level'] = 1;
-  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
-  $settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
+//  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+//  $settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
 
   // Apply changes to the container configuration to better leverage Redis.
   // This includes using Redis for the lock and flood control systems, as well
@@ -53,35 +58,35 @@ if (isset($_ENV['REDIS_HOST'])) {
 
   // Manually add the classloader path, this is required for the container cache bin definition below
   // and allows to use it without the redis module being enabled.
-//  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+  $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
 
   // Use redis for container cache.
   // The container cache is used to load the container definition itself, and
   // thus any configuration stored in the container itself is not available
   // yet. These lines force the container cache to use Redis rather than the
   // default SQL cache.
-//  $settings['bootstrap_container_definition'] = [
-//    'parameters' => [],
-//    'services' => [
-//      'redis.factory' => [
-//        'class' => 'Drupal\redis\ClientFactory',
-//      ],
-//      'cache.backend.redis' => [
-//        'class' => 'Drupal\redis\Cache\CacheBackendFactory',
-//        'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
-//      ],
-//      'cache.container' => [
-//        'class' => '\Drupal\redis\Cache\PhpRedis',
-//        'factory' => ['@cache.backend.redis', 'get'],
-//        'arguments' => ['container'],
-//      ],
-//      'cache_tags_provider.container' => [
-//        'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
-//        'arguments' => ['@redis.factory'],
-//      ],
-//      'serialization.phpserialize' => [
-//        'class' => 'Drupal\Component\Serialization\PhpSerialize',
-//      ],
-//    ],
-//  ];
+  $settings['bootstrap_container_definition'] = [
+    'parameters' => [],
+    'services' => [
+      'redis.factory' => [
+        'class' => 'Drupal\redis\ClientFactory',
+      ],
+      'cache.backend.redis' => [
+        'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+        'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+      ],
+      'cache.container' => [
+        'class' => '\Drupal\redis\Cache\PhpRedis',
+        'factory' => ['@cache.backend.redis', 'get'],
+        'arguments' => ['container'],
+      ],
+      'cache_tags_provider.container' => [
+        'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+        'arguments' => ['@redis.factory'],
+      ],
+      'serialization.phpserialize' => [
+        'class' => 'Drupal\Component\Serialization\PhpSerialize',
+      ],
+    ],
+  ];
 }
