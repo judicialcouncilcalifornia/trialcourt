6cd3ade87f1e6c82aaee60f6b6e45b30a919ec91
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-12-20 17:47:32 -0800

    clean up permissions and missing log files

--- .devops/reverseProxy/init_container.sh
+++ .devops/reverseProxy/init_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
 [ -e "/home/site/nginx.conf" ] && cp "/home/site/nginx.conf" "/etc/nginx/nginx.conf"
+test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
+
 cd /usr/bin/
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
