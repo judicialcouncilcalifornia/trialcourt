d194aa7b9258261009488eb59d96b5040c904acc
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-12-20 16:01:28 -0800

    use debian nginx

--- .devops/reverseProxy/init_container.sh
+++ .devops/reverseProxy/init_container.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# set -e
-
-echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
-
-test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
-test ! -d "$NGINX_LOG_DIR" && echo "INFO: Log folder for nginx/php not found. creating..." && mkdir -p "$NGINX_LOG_DIR"
-test ! -e /home/50x.html && echo "INFO: 50x file not found. createing..." && cp /usr/share/nginx/html/50x.html /home/50x.html
-
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-echo "Starting SSH ..."
-echo "Starting php-fpm ..."
-echo "Starting Nginx ..."
-
-cd /usr/bin/
-supervisord -c /etc/supervisord.conf
-
+[ -e "/home/site/nginx.conf" ] && cp "/home/site/nginx.conf" "/etc/nginx/nginx.conf"
+/usr/bin/supervisord
