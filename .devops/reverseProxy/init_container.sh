8970953453cfa037396991f68bdacfc2356bf4aa
Ivan Bueno <ivan.bueno@jud.ca.gov>
2022-12-20 18:26:58 -0800

    add another

--- .devops/reverseProxy/init_container.sh
+++ .devops/reverseProxy/init_container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
-[ -e "/home/site/nginx.conf" ] && cp "/home/site/nginx.conf" "/etc/nginx/nginx.conf"
 test ! -d "$SUPERVISOR_LOG_DIR" && echo "INFO: $SUPERVISOR_LOG_DIR not found. creating ..." && mkdir -p "$SUPERVISOR_LOG_DIR"
 
 cd /usr/bin/
